5712574344a13428bf9e7874c1338e4438d5ff33
tarunkumardx <tarun@digitalxplode.com>
2024-09-05 11:46:58 +0530

    showmore option on mobile filter remoded

--- src/stories/form-inputs/checkbox/index.tsx
+++ src/stories/form-inputs/checkbox/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface CheckBoxProps {
 	name?: string;
@@ -33,7 +33,11 @@ const CheckBox: React.FC<CheckBoxProps> = ({
   const handleShowMore = () => {
     setShowMore(!showMore);
   };
-
+  useEffect(()=>{
+    if(window.innerWidth < 768){
+      setShowMore(true)
+    }
+  },[])
   return (
     <div className={`form-group ${className}`}>
       {label && <label className="form-label mb-1 d-block">{label}</label>}
